a7cf27924e3695cfbc67a0695c0d1e6c07401c47
kotecbr@outlook.com <kotecbr@outlook.com>
2024-05-29 21:42:49 -0300

    Perfil Atualizado

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -27,7 +27,7 @@ function Navbar() {
                         <Link to='/postagens' className='hover:underline'>Postagens</Link>
                         <Link to='/temas' className='hover:underline'>Temas</Link>
                         <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link>
-                        Perfil
+                        <Link to='/perfil' className='hover:underline'>Perfil</Link>
 
                         <Link to="" onClick={logout} className="hover:underline">Sair</Link>
                     </div>
