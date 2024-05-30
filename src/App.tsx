5fce1fd6d50652ab4169bf37809d8876e3c88d5b
kotecbr@outlook.com <kotecbr@outlook.com>
2024-05-29 21:27:05 -0300

    Alterado estilo das imagens de login e cadastrar

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import ListaPostagens from './components/postagem/listapostagens/ListaPostagens'
 import { ToastContainer } from 'react-toastify'
 
 import 'react-toastify/dist/ReactToastify.css';
+import Perfil from './pages/perfil/Perfil'
 
 function App() {
     return (
@@ -36,6 +37,7 @@ function App() {
                             <Route path="/cadastrarpostagem" element={<FormPostagem />} />
                             <Route path="/editarpostagem/:id" element={<FormPostagem />} />
                             <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+                            <Route path="/perfil" element={<Perfil/>} />
                         </Routes>
                     </div>
                     <Footer />
